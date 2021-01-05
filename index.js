9a864abd9d7df92251501965277adfd51a669b12
Androz2091 <androz2091@gmail.com>
2021-01-05 19:04:32 +0100

    feat: update code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({
+    partials: [ 'CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'USER' ]
+});
 const config = require("./config.js");
 
 client.login(config.token);
@@ -7,6 +9,8 @@ client.login(config.token);
 client.on("ready", () => {
     console.log("Ready.");
 });
+
+let guild = null;
 
 client.on("guildMemberAdd", (member) => {
     const welcomeChannel = member.guild.channels.cache.find((channel) => channel.name === "welcome");
@@ -17,21 +21,29 @@ client.on("guildMemberAdd", (member) => {
         welcomeChannel.send(":bust_in_silhouette: New user: "+member.toString()+"\n\nWelcome in the server, "+member.user.tag+". I guess you're here to use the Atlanta emojis on your own instance. You now have the `MANAGE_GUILD` permissions, so you can add your bot on the server. Once it's added, it will have access to all the emojis. Please disable all the features that could update the guild (like the icon) or delete invites. If one of these actions are made by you or the bot, you and your bot will automatically be banned.\nWell, have a nice day.\nThe Guardian.");
         member.roles.add(config.users);
     }
+    guild = welcomeChannel.guild;
 });
 
-client.on("guildUpdate", (oldGuild, newGuild) => {
-    newGuild.fetchAuditLogs().then((logs) => {
+const fetchLogsAndBan = () => {
+    if (!guild) return;
+    guild.fetchAuditLogs().then((logs) => {
         if(
             (logs.entries.first().action === "GUILD_UPDATE" ||
             logs.entries.first().action === "INVITE_DELETE") &&
-            logs.entries.first().executor.id !== newGuild.owner.id &&
             logs.entries.first().executor.id !== client.user.id
         ){
-            const welcomeChannel = newGuild.channels.cache.find((channel) => channel.name === "welcome");
+            if (logs.entries.first().executor.id !== newGuild.owner.id) {
+                guild.owner.send(":warning: "+logs.entries.first().executor.tag+" was not banned because he was allowed to.");
+                break;
+            }
+            const welcomeChannel = guild.channels.cache.find((channel) => channel.name === "welcome");
             welcomeChannel.send(":warning: "+logs.entries.first().executor.tag+" was banned automatically.");
-            newGuild.members.ban(logs.entries.first().executor.id);
-            newGuild.owner.send(":warning: "+logs.entries.first().executor.tag+" has modified "+newGuild.name+"!");
-            newGuild.setIcon(null);
+            guild.members.ban(logs.entries.first().executor.id);
+            guild.owner.send(":warning: "+logs.entries.first().executor.tag+" has modified "+guild.name+"!");
+            guild.setIcon(null);
         }
     });
-});
+};
+
+client.on("inviteDelete", () => fetchLogsAndBan());
+client.on("guildUpdate", () => fetchLogsAndBan());
