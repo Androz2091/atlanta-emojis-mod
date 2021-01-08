6821960983f86ceb8c531ab3dd4a2083d90c54f2
Androz2091 <androz2091@gmail.com>
2021-01-08 16:39:32 +0100

    :sparkles: Add adder info

--- index.js
+++ index.js
@@ -15,8 +15,17 @@ let guild = null;
 client.on("guildMemberAdd", (member) => {
     const welcomeChannel = member.guild.channels.cache.find((channel) => channel.name === "welcome");
     if(member.user.bot){
-        welcomeChannel.send(":robot: New bot: "+member.toString());
-        member.roles.add(config.bots);
+
+        member.guild.fetchAuditLogs().then((logs) => {
+            let adder = '';
+            if (logs.entries.first().action === "BOT_ADD") {
+                adder = logs.entries.first().executor.id;
+            }
+
+            welcomeChannel.send(":robot: New bot: "+member.toString()+(adder ? '. Added by <@'+adder : '>'));
+            member.roles.add(config.bots);
+        })
+        
     } else {
         welcomeChannel.send(":bust_in_silhouette: New user: "+member.toString()+"\n\nWelcome in the server, "+member.user.tag+". I guess you're here to use the Atlanta emojis on your own instance. You now have the `MANAGE_GUILD` permissions, so you can add your bot on the server. Once it's added, it will have access to all the emojis. Please disable all the features that could update the guild (like the icon) or delete invites. If one of these actions are made by you or the bot, you and your bot will automatically be banned.\nWell, have a nice day.\nThe Guardian.");
         member.roles.add(config.users);
